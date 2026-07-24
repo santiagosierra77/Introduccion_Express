11684de032cda96a9953176af8aae88cab26c1b4
Santiago <santi17@ejemplo.com>
2026-07-24 11:53:55 -0500

    Configuracion de variables parte 1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const app = express();  
-const port = 3000; 
+const puerto = process.env.PUERTO || 3000; 
 
-app.get("/", (_, res) => { 
+app.get("/", function(req, res) { 
     res.send('Aprendiendo express,ficha 3407181, parchados de la vida jajaja :), esta re breveeee, esta meloooo'); 
 });
 
-app.listen(port, () => {
-    console.log( `Servidor en funcionamiento en el puerto: `); 
+app.listen(puerto, () => {
+    console.log( `Servidor en funcionamiento en el puerto: ${puerto}`); 
 }); 
